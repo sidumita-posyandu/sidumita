fb663d9fdb105a878b2846f977b829caf66739c0
sidumita <119862512+sidumita-posyandu@users.noreply.github.com>
2023-07-10 14:00:32 +0800

    Update edit.blade.php

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -4,7 +4,7 @@
 <div class="row">
     <div class="col-lg-12 margin-tb">
         <div class="pull-left">
-            <h2>Tambah User</h2>
+            <h2>Edit User</h2>
         </div>
         <div class="pull-right">
             <a class="btn btn-secondary btn-sm mb-3" href="{{ route('users.index') }}"><i
